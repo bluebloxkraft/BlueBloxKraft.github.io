815964c3d83a73210e1d22fbf1be469e8e8c2c09
BlueBloxKraft <94107880+BlueBloxKraft@users.noreply.github.com>
2022-04-09 10:45:54 +0530

    Add files via upload

---
+++ assets/bbk/scripts/script.js
@@ -0,0 +1,12 @@
+var nameDiscord = "BlueBloxKraft";
+var discDiscrim = "#1812";
+var ytLink = "https://www.youtube.com/channel/UC-KLEozPWQPxLn3aMmcTdEA";
+var githubLink = "https://github.com/BlueBloxKraft";
+var mcfontDir = "assets/bbk/fonts/mc_font/ttf";
+
+document.addEventListener("keypress", 
+function(e) {
+    if(e.key == "b") {
+           window.open(ytLink);
+       }
+})
