6516d77da95e3c97da5d4d8d767e56816b09f2f2
BlueBloxKraft <94107880+BlueBloxKraft@users.noreply.github.com>
2022-04-25 15:06:03 +0530

    Update script.js

--- assets/bbk/scripts/script.js
+++ assets/bbk/scripts/script.js
@@ -2,6 +2,9 @@ var nameDiscord = "BlueBloxKraft";
 var discDiscrim = "#1812";
 var ytLink = "https://www.youtube.com/channel/UC-KLEozPWQPxLn3aMmcTdEA";
 var githubLink = "https://github.com/BlueBloxKraft";
+var twitchLink = "https://twitch.tv/BlueBloxKraft";
+var redditLink = "https://www.reddit.com/user/BlueBloxKraft";
+var discordLink = "https://discord.gg/xUj3Pvtn2n";
 var mcfontDir = "assets/bbk/fonts/mc_font/ttf";
 
 document.addEventListener("keypress", 
@@ -10,3 +13,18 @@ function(e) {
            window.open(ytLink);
        }
 })
+
+var url = window.location.search;
+var redirect = new URLSearchParams(url).get("redirect");
+
+if(redirect == "yt" || redirect == "youtube") {
+    window.open(ytLink);
+} else if(redirect = "github") {
+    window.open(githubLink);
+} else if(redirect = "twitch") {
+    window.open(twitchLink);
+} else if(redirect = "reddit") {
+    window.open(redditLink);
+} else if(redirect = "discord") {
+    window.open(discordLink);
+}
