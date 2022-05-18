818866d69ab5e52b326f1a2c0c0ac13f7d5bafb2
BlueBloxKraft <94107880+BlueBloxKraft@users.noreply.github.com>
2022-05-18 21:11:13 +0530

    Update script.js

--- assets/bbk/scripts/script.js
+++ assets/bbk/scripts/script.js
@@ -16,15 +16,3 @@ function(e) {
 
 var url = window.location.search;
 var redirect = new URLSearchParams(url).get("redirect");
-
-if(redirect == "yt" || redirect == "youtube") {
-    window.open(ytLink);
-} else if(redirect = "github") {
-    window.open(githubLink);
-} else if(redirect = "twitch") {
-    window.open(twitchLink);
-} else if(redirect = "reddit") {
-    window.open(redditLink);
-} else if(redirect = "discord") {
-    window.open(discordLink);
-}
