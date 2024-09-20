1c0a6e24f5664de0be03e761483c1e1360f335f4
bbk <bbk@BlueBloxKraft-PC>
2024-09-20 12:29:29 +0530

    some changes

--- assets/bbk/scripts/script.js
+++ assets/bbk/scripts/script.js
@@ -1,11 +1,4 @@
-var nameDiscord = "BlueBloxKraft";
-var discDiscrim = "#1812";
 var ytLink = "https://www.youtube.com/channel/UC-KLEozPWQPxLn3aMmcTdEA";
-var githubLink = "https://github.com/BlueBloxKraft";
-var twitchLink = "https://twitch.tv/BlueBloxKraft";
-var redditLink = "https://www.reddit.com/user/BlueBloxKraft";
-var discordLink = "https://discord.gg/xUj3Pvtn2n";
-var mcfontDir = "assets/bbk/fonts/mc_font/ttf";
 
 document.addEventListener("keypress", 
 function(e) {
@@ -13,3 +6,36 @@ function(e) {
            window.open(ytLink);
        }
 })
+
+document.addEventListener('DOMContentLoaded', () => {
+    const logo = document.getElementById('theLogo');
+    const container = document.querySelector('.logo');
+
+    let posX = Math.random() * (window.innerWidth - 100);
+    let posY = Math.random() * (window.innerHeight - 100);
+    let speedX = 2;
+    let speedY = 2;
+
+    function animate() {
+        posX += speedX;
+        posY += speedY;
+
+        const logoW = logo.clientWidth;
+        const logoH = logo.clientHeight;
+
+        if (posX + logoW - 80 >= window.innerWidth || posX - 60 <= 0) {
+            speedX = -speedX;
+        }
+
+        if (posY + logoH - 25 >= window.innerHeight || posY + 25 <= 0) {
+            speedY = -speedY;
+        }
+
+        logo.style.left = posX + 'px';
+        logo.style.top = posY + 'px';
+
+        requestAnimationFrame(animate);
+    }
+
+    animate();
+});
